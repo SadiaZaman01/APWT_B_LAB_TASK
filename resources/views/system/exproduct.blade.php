ae3918d6ebfd97855d138b0021d0f2fba6d3d936
Sadia Zaman <sadia.sorno.zaman@gmail.com>
2021-06-23 23:26:40 +0600

    task done

---
+++ resources/views/system/exproduct.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Social Sales Page</title>
+</head>
+<body>
+	<h2>Existing Product Page</h2>
+
+	
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +14,32 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login','LoginController@index')->name('login.index');
+Route::post('/login','LoginController@verify')->name('login.verify');
+
+Route::get('/registration', 'RegistrationController@index')->name('registration.index');
+Route::post('/registration', 'RegistrationController@insert')->name('registration.insert');
+
+Route::get('/system', 'SystemController@index')->name('system.index');
+
+
+Route::get('/system/sales', 'SalesController@index')->name('Sales.index');
+
+Route::get('/system/sales/physical_store', 'PhysicalController@index')->name('Physical.index');
+Route::get('/system/sales/social_media', 'SocialController@index')->name('Social.index');
+Route::get('/system/sales/ecommerce', 'EcommerceController@index')->name('Ecommerce.index');
+
+Route::get('/system/product_management', 'ProductController@index')->name('Product.index');
+
+Route::get('/system/product_management/existing_products', 'ProductController@existingIndex')->name('Product.existingIndex');
+Route::get('/system/product_management/upcoming_products', 'ProductController@upcomingIndex')->name('Product.upcomingIndex');
+Route::get('/system/product_management/add_product', 'ProductController@addIndex')->name('Product.addIndex');
+Route::post('/system/product_management/add_product', 'ProductController@addProduct')->name('Product.addProduct');
+
+Route::get('/system/product_management', 'ProductController@index')->name('Product.index');
+
+Route::get('/system/product_management/existing_products', 'ProductController@existingIndex')->name('Product.existingIndex');
+Route::get('/system/product_management/upcoming_products', 'ProductController@upcomingIndex')->name('Product.upcomingIndex');
+Route::get('/system/product_management/add_product', 'ProductController@addIndex')->name('Product.addIndex');
+Route::post('/system/product_management/add_product', 'ProductController@addProduct')->name('Product.addProduct');
